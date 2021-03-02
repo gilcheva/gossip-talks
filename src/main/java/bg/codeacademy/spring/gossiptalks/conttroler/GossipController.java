0c8eabe9622b12f4affe6cd1d22de68671430ce7
lenovo <novakova.elitca@gmail.com>
2021-03-02 12:22:30 +0200

    Create controllers

---
+++ src/main/java/bg/codeacademy/spring/gossiptalks/conttroler/GossipController.java
@@ -0,0 +1,13 @@
+package bg.codeacademy.spring.gossiptalks.conttroler;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1/gossip")
+@Validated
+public class GossipController {
+
+}
